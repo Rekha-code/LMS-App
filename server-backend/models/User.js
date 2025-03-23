67623c0c938b33433305f92df182ee6b514b4e24
rekha_gorde <rekha.gorde8@gmail.com>
2025-03-23 17:47:29 +0530

    user creation

--- server-backend/models/User.js
+++ server-backend/models/User.js
@@ -1,20 +1,21 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema(
   {
-      _id :{ type:String, required:true },
-      name:{type:String, required:true },
-      email:{type:String, required:true },
-      imageUrl:{type:String, required:true },
-      enrolledCourses:[
-        {
-          type:mongoose.Schema.Types.ObjectId,
-          ref:'Course'
-        }
-      ],
-  },{timestamps:true});
+    clerkId: { type: String, required: true, unique: true }, // Store Clerk ID separately
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true }, // Emails should be unique
+    imageUrl: { type: String, required: true },
+    enrolledCourses: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Course',
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
-  const User = mongoose.model('User',userSchema);
+const User = mongoose.model('User', userSchema);
 
-  export default User
-
+export default User;
